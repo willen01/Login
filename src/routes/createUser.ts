45c0c14077efc86dda77ea91fb45c961094978eb
willen <willen.sesantos@gmail.com>
2023-01-09 22:30:08 -0300

    chore: Altera endpoint de cadastro de usuário

--- src/routes/createUser.ts
+++ src/routes/createUser.ts
@@ -3,7 +3,7 @@ import { createUserController } from "../modules/register/useCases/createUser";
 
 const createUserRoute = Router();
 
-createUserRoute.post("/register", async (request, response) => {
+createUserRoute.post("/api/user/register", async (request, response) => {
   await createUserController.handle(request, response);
 });
 
