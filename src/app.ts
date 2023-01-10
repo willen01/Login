85d040db476e3dfab437d89c35a92585b64601dd
willen <willen.sesantos@gmail.com>
2023-01-09 22:28:38 -0300

    docs: Documenta endpoint de cadastro de usuário com swagger

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import "dotenv/config";
 import { router } from "./routes";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
+
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
 app.use(router);
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Server road on port ${port}`);
