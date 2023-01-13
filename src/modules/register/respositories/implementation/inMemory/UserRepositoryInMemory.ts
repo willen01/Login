26bba176735616be799ef7ae09103cd9017da1f1
willen <willen.sesantos@gmail.com>
2023-01-13 20:10:50 -0300

    chore: Implementa método updatePassword
    implementação necessária para os testes unitários rodarem adequadamente

--- src/modules/register/respositories/implementation/inMemory/UserRepositoryInMemory.ts
+++ src/modules/register/respositories/implementation/inMemory/UserRepositoryInMemory.ts
@@ -1,3 +1,4 @@
+import { CustomError } from "../../../../../errors/custon.error";
 import { User } from "../../../entities/user.entity";
 import { IUserRepository } from "../../user.repository";
 
@@ -15,5 +16,14 @@ export class UserRepositoryInMemory implements IUserRepository {
 
   async findUserByEmail(email: string): Promise<User | undefined> {
     return this.user.find((user) => user.email == email);
+  }
+
+  async updatePassword(userId: string, password: string): Promise<boolean> {
+    const userIndex: number = this.user.findIndex((user) => user.id == userId);
+
+    if (userIndex == -1) return false;
+
+    this.user[userIndex].password = password;
+    return true;
   }
 }
